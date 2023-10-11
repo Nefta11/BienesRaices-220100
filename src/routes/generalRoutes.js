dabf883971dc49ddc991cf400e9702b0cf50e76b
netf_ <neftaliarturohernandez@gmail.com>
2023-10-10 23:18:32 -0600

    Actualizacion

--- src/routes/generalRoutes.js
+++ src/routes/generalRoutes.js
@@ -1,15 +1,15 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
-const router= express.Router();
+const router = express.Router();
 
 //router.get('/',(request,response)=> response.send("Hola web desde GET")) // descomentar para revisar practica de verbos htttp
-router.get('/',(request,response)=> response.render("layout/index.pug",{page:"Home"}))
+router.get('/', (request, response) => response.render("layout/index.pug", {page: "Home"})); 
 
 router.get('/quienEres',(request,response)=> response.send("Soy tu primera App web en arquitectura Soa (service object oriented)."))
 
 router.get('/queUsas',(request,response)=> response.send("Estoy construida en el lenguaje de programacion JavaScrip, y utilizo el microservidor de Express."))
 
-router.get('/misDatos',(rq,re) => re.json({nombre: "Su nombre completo aqui", fechaNacimiento:"2000-04-30", matricula:"220100"}))
+router.get('/misDatos',(rq,re) => re.json({nombre: "Neftali Arturo Hernandez Vergara", fechaNacimiento:"2000-04-30", matricula:"220100"}))
 
 router.get('/diaHoy', (req, res) => res.json(`El dia de hoy es ${new Date().toLocaleDateString()} y la hora actual es ${new Date().toLocaleTimeString()}` ));
 
