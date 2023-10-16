ebfd86874e7594fc65a246257cec5293f6e4d793
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-16 15:13:39 -0600

    Actualizado...

--- src/config/bd.js
+++ src/config/bd.js
@@ -1,8 +1,18 @@
-import Sequelize from "sequelize"
+import Sequelize from "sequelize";
 
 
 const bd = new Sequelize ("rs-220100","Nefta11","1234567",{
-host:"",
+host:"localhost",
 port:"3307",
+dialect:"mysql",
+define:{
+    timestamps:true
+},
+pool:{
+    max:5,//usuarios
+    min:0,
+    acquire: 30000,//Eltiempo que va a esperar a recibir una petición
+    idle:10000,//suspende la conexion mientras no se ingresa nada.
+}
 
 })
