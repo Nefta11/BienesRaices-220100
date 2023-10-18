4425c9dc5afccd11f391b7b5886623a6680e598f
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-18 09:05:31 -0600

    Actualializado...

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -20,13 +20,21 @@ const formPasswordRecovery = (request,response) => {
 
 
 const insertUser= async (request,response) =>{
-    response.redirect('/login')
+//    response.redirect('/login')
     console.log("Intentando registrar los datos del usuario en la Base de Datos")
     console.log(`Nombre: ${request.body.name}`)
     console.log(`password: ${request.body.password}`)
-    await check("name").notEmty().withMessage("This fieldis required").run(request)
+    await check("name").notEmpty().withMessage("This fieldis required").run(request)
+    await check("email").notEmpty().withMessage("This fieldis required").isEmail().withMessage("This is not emailformat").run(request)
+    await check("password").notEmpty().withMessage("This fieldis required").isLength({min:8}).withMessage("The password must have 8 characterat lest ").run(request)
+    await check("password2").notEmpty().withMessage("This fieldis required").isLength({min:8}).withMessage("The password most have 8characters at least").equals("password").withMessage("Bothpasswords felds must be the same").run(request)
     response.json(validationResult(request))
-    let nemUser= await User.create(request.body)
+    console.log(`Se encontraron: ${validationResult.length} errores de validación`)
+    
+    let resultadoValidacion=validationResult(request)
+    if (resultadoValidacion.isEmpty()){
+
+    let nemUser= await User.create(request.body)}
 
 }
 
