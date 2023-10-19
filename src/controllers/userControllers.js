fa6e6aeb815382c1d93cbd532b46882ccf4f3445
netf_ <neftaliarturohernandez@gmail.com>
2023-10-19 14:52:25 -0600

    Actualizado...

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,44 +1,53 @@
 import User from "../models/User..js"
 import { check, validationResult } from 'express-validator'
 
-const formLogin = (request,response) => {
-    response.render("auth/login.pug",{
-        page:"Login",
-    isLogged : false})}
+const formLogin = (request, response) => {
+    response.render("auth/login.pug", {
+        page: "Login",
+        isLogged: false
+    })
+}
 
-const formRegister = (request,response) => {
+const formRegister = (request, response) => {
     response.render("auth/register.pug",
-    {
-        page : "Creating a new account..."
-    })}
+        {
+            page: "Creating a new account..."
+        })
+}
 
-const formPasswordRecovery = (request,response) => {
+const formPasswordRecovery = (request, response) => {
     response.render("auth/password-recovery.pug",
-    {
-        page:"Password Recovery"
-    })}    
+        {
+            page: "Password Recovery"
+        })
+}
 
 
-const insertUser= async (request,response) =>{
-//    response.redirect('/login')
+const insertUser = async (request, response) => {
+    //  response.redirect('/login')
     console.log("Intentando registrar los datos del usuario en la Base de Datos")
     console.log(`Nombre: ${request.body.name}`)
     console.log(`password: ${request.body.password}`)
     await check("name").notEmpty().withMessage("This fieldis required").run(request)
     await check("email").notEmpty().withMessage("This fieldis required").isEmail().withMessage("This is not emailformat").run(request)
-    await check("password").notEmpty().withMessage("This fieldis required").isLength({min:8}).withMessage("The password must have 8 characterat lest ").run(request)
-    await check("password2").notEmpty().withMessage("This fieldis required").isLength({min:8}).withMessage("The password most have 8 haracters at least").equals(request.body.password).withMessage("Both passwords felds must be the same").run(request)
+    await check("password").notEmpty().withMessage("This fieldis required").isLength({ min: 8 }).withMessage("The password must have 8 characterat lest ").run(request)
+    await check("password2").notEmpty().withMessage("This fieldis required").isLength({ min: 8 }).withMessage("The password most have 8 haracters at least").equals(request.body.password).withMessage("Both passwords felds must be the same").run(request)
     //response.json(validationResult(request))
     console.log(`Se encontraron: ${validationResult.length} errores de validación`)
-    
-    let resultadoValidacion=validationResult(request)
-    if (resultadoValidacion.isEmpty()){
 
-    let nemUser= await User.create(request.body)}
-    else{
-        response.render("auth/register.pug",{
-            page:"Creating a new account...",
-            errors:resultadoValidacion.array()
+    let resultadoValidacion = validationResult(request)
+    if (resultadoValidacion.isEmpty()) {
+
+        let nemUser = await User.create(request.body)
+        response.json({ sucess: true, message: "User registered successfully" });
+    } else {
+        response.render("auth/register.pug", {
+            page: "Creating a new account...",
+            errors: resultadoValidacion.array(),
+            user:{
+                 name: request.body.name,
+                 email: request.body.email
+        }
         })
     }
 
--- src/models/User..js
+++ src/models/User..js
@@ -1,31 +1,31 @@
-import {DataTypes} from "sequelize"
+import { DataTypes } from "sequelize"
 import db from '../config/bd.js'
 
 
-const User=db.define('tbb_users' ,{
+const User = db.define('tbb_users', {
     name: {
         type: DataTypes.STRING,
-        allowNull:false
+        allowNull: false
     },
 
     email: {
         type: DataTypes.STRING,
-        allowNull:false,
-        unique:true
+        allowNull: false,
+        unique: true
     },
     password: {
         type: DataTypes.STRING,
-        allowNull:false,
+        allowNull: false,
     },
     token: {
-       type: DataTypes.STRING,
-       default: ""
+        type: DataTypes.STRING,
+        default: ""
     },
 
-    verified:{
-        type:DataTypes.BOOLEAN,
-        default:false
-    },
+    verified: {
+        type: DataTypes.BOOLEAN,
+        default: false
+    },  
 
 })
 
