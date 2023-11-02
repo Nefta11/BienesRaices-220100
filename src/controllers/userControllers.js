83fb7d3cf12d32c17964bfa8851d9b1ccfe54c13
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-01 20:12:05 -0600

    Actualizado siuuu...

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -11,6 +11,15 @@ const formLogin = (request, response) => {
     })
 }
 
+const formPasswordUpdate = (request, response) => {
+    
+    response.render("../views/auth/password-update.pug", {
+        isLogged: false,
+        page: "Password update",
+        
+    })
+}
+
 const formRegister = (request, response) => {
     response.render("auth/register.pug",
         {
@@ -44,7 +53,7 @@ const insertUser = async (request, response) => {
 
     console.log(userExists)
 
-    const {name,email,password} =request.body
+    const { name, email, password } = request.body
 
     if (userExists) {
         response.render("auth/register.pug", {
@@ -58,18 +67,18 @@ const insertUser = async (request, response) => {
     }
 
     else if (resultadoValidacion.isEmpty()) {
-        const token=generateToken()
+        const token = generateToken()
         let newUser = await User.create({
-            name,email,password,token
+            name, email, password, token
         })
 
-        emailRegister({email,name,token})
-        
+        emailRegister({ email, name, token })
+
         response.render("templates/message.pug", {
             page: "New Account Created",
             email: email
         });
-        
+
     } else {
         response.render("auth/register.pug", {
             page: "Creating a new account...",
@@ -82,58 +91,79 @@ const insertUser = async (request, response) => {
     }
 
 }
-const confirmAccount = async (req,res) =>{
+const confirmAccount = async (req, res) => {
     //TODO Verificar token
-    const tokenRecived= req.params.token
-    const userOwner= await User.findOne({where:{token:tokenRecived}})
-    
-    if (!userOwner){
+    const tokenRecived = req.params.token
+    const userOwner = await User.findOne({ where: { token: tokenRecived } })
+
+    if (!userOwner) {
         console.log("El token no existe")
-        res.render('auth/confirm-account',{
-            page:'status verification',
+        res.render('auth/confirm-account', {
+            page: 'status verification',
             error: true,
-            msg:'we have found some inssues in account verification.',
+            msg: 'we have found some inssues in account verification.',
         })
-    }    
-    else{
+    }
+    else {
         console.log("El token existe")
         userOwner.token = null;// Actualice de cadena vacia a null ya que me marcaba error de duplicidad.
-        userOwner.verified =true;
+        userOwner.verified = true;
         await userOwner.save();//esta operacion realiza el update en la base de datos
-        res.render('auth/confirm-account',{
-            page:'status verification',
+        res.render('auth/confirm-account', {
+            page: 'status verification',
             error: false,
-            msg:'Your account has been confirmed succesfuly.'
+            msg: 'Your account has been confirmed succesfuly.'
         })
     }
 }
 
-const updatePassword = (req,res) => {
+const updatePassword = (req, res) => {
 
 }
 
-const emailChangePassword = async (req,res) => {
-console.log(`El usuario ha solicitado cambiar su contraseña por lo que se le enviara un correo electronico a ${req.body.email} con la liga para actualizar su contraseña `)    
+const emailChangePassword = async (req, res) => {
+    console.log(`El usuario ha solicitado cambiar su contraseña por lo que se le enviara un correo electronico a ${req.body.email} con la liga para actualizar su contraseña `)
 
-await check("email").notEmpty().withMessage("This fieldis required").isEmail().withMessage("This is not emailformat").run(req)
+    await check("email").notEmpty().withMessage("Your email is required").isEmail().withMessage("This is not email format").run(req)
+    let resultadoValidacion = validationResult(req)
+    const { email, password } = req.body
 
-let resultadoValidacion = validationResult(request)
-const userExists = await User.findOne({ where: { email: request.body.email } })
 
-const {name,email,password} =request.body
+    if (resultadoValidacion.isEmpty()) {
+        const userExists = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
 
-    if (userExists) {
-        response.render("auth/register.pug", {
-            page: "Creating a new account...",
-            errors: [{ msg: `El usuario con: ${request.body.email} already exist` }],
-            user: {
-                name: request.body.name,
-                email: request.body.email
-            },
-        })
+    
+
+    if (!userExists) {
+        console.log(`El usuario: ${email} que esta intentando recuperar su contraseña no existe`);
+        res.render("templates/message.pug", {
+            page: "User nor found",
+            message: `The user associated with: ${email} does not exist in database`,
+            type: "error"
+        });
+
     }
-return 0;
+    else{
+        //TODO generar un nuevo token
+        const token =generateToken();
+        userExists.token= token;
+        userExists.save();
+    }
+}
+else {
+    res.render("auth/recovery.pug", ({
+    page: "Password recovery",
+    errors: resultadoValidacion.array(),
+    email
+    }))
+}
+
+    return 0;
 }
 
 
-export { formLogin, formRegister, formPasswordRecovery, insertUser, confirmAccount,updatePassword , emailChangePassword };
+export { formLogin, formRegister, formPasswordRecovery, insertUser, confirmAccount, updatePassword, emailChangePassword,formPasswordUpdate};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { formLogin, formRegister,formPasswordRecovery, insertUser, confirmAccount,emailChangePassword,updatePassword} from "../controllers/userControllers.js";
+import { formLogin, formRegister,formPasswordRecovery, insertUser, confirmAccount,formPasswordUpdate,emailChangePassword,updatePassword} from "../controllers/userControllers.js";
 const router = express.Router();
 
 router.get("/", formLogin)
 router.get("/register", formRegister)
 router.get("/password-recovery", formPasswordRecovery)
 router.post("/password-recovery", emailChangePassword)
+router.get("/update-password/:token", formPasswordUpdate);
 router.post("/update-password", updatePassword)
 router.post("/register",insertUser)
 router.get("/confirm/:token",confirmAccount)
