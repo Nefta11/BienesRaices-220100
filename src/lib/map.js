37d270827c82f833d71dd84d234c0feaeb52a870
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-15 08:12:43 -0600

    UPDATE

--- src/lib/map.js
+++ src/lib/map.js
@@ -4,5 +4,9 @@
     const map = L.map('map').setView([lat, lng], 16);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);        
+    }).addTo(map);  
+    L.marker([lat, lng]).addTo(map)
+    .bindPopup('MI CASA.<br> NEFTAHOUSE.')
+    .openPopup();      
 })();
+
