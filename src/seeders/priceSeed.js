0f4a7a3a5aef95d11ed73c916d8b214930857fae
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-21 14:26:30 -0600

    Update..

---
+++ src/seeders/priceSeed.js
@@ -0,0 +1,21 @@
+const prices =[{
+    name:'< $USD 10,000'
+},
+{
+    name:'$USD 10,000 - $USD 30,000'
+},
+{
+    name:'$USD 31,000 - $USD 50,000'
+},
+{
+    name:'$USD 51,000 - $USD 75,000'
+},
+{
+    name:'$USD 151,000 - $USD 200,000'
+},
+{
+    name:'>$USD 200,000'
+},
+]
+
+export default prices;
