71f9fb4d8540b95dec0618ab4b75c7186ea4424c
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-22 09:47:30 -0600

    Update..

--- src/controllers/propertyControllers.js
+++ src/controllers/propertyControllers.js
@@ -1,12 +1,16 @@
-import Property from "../models/property";
+import Price from "../models/price";
 import Category from "../models/category";
 
 
-const formProperty =(req,res) =>{
+const formProperty = async (req,res) =>{
+    //TODO: Obtener los precios y categorias actuales para enviarlos a el formulario (vistas)
+
+    const [categories, prices]= await Promise.all([Category.findAll(),Price.findAll()])
+
     res.render('properties/create.pug',{
         page:'New property',
-        showHeader:true
-        
+        showHeader:true,
+        categories,prices
     });
 }
 
