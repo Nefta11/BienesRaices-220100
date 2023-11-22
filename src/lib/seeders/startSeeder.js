e163677b1630517411ec462b3dc2f4cd01360f68
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-22 08:35:06 -0600

    Update..

--- src/lib/seeders/startSeeder.js
+++ src/lib/seeders/startSeeder.js
@@ -13,11 +13,15 @@ const importData = async () => {
     await Category.bulkCreate(categories)
     console.log("Se han importado los datos de las tabla catalogo de manera correcta ")
     exit()
-    
+
     }catch(error){
         console.log(error);
         exit(1);
     }
+}
+
+if (process.argv[2] === "-i"){
+    importData()
 }
 
 
