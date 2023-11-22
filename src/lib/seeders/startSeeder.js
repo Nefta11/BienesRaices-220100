7585490f912e93b62eff2213334bdcdf6f00bcd3
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-22 08:22:47 -0600

    Update

--- src/lib/seeders/startSeeder.js
+++ src/lib/seeders/startSeeder.js
@@ -3,4 +3,15 @@ import Category from '../../models/category.js'
 import categories from './categorySeed.js'
 import db from '../../config/bd.js'
 
+const importData = async () => {
+    try{
+        //TODO: Autenticar
+        //TODO: Generar las columnas
+        //TODO: Importar los datos
+    }catch(error){
+        console.log(error);
+        exit(1);
+    }
+}
+
 
