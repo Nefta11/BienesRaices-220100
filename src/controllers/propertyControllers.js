cf1c3e93c3ece7f787b94482a8f8809d99b85d14
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-23 15:27:24 -0600

    Update..

--- src/controllers/propertyControllers.js
+++ src/controllers/propertyControllers.js
@@ -20,7 +20,13 @@ await check("title").notEmpty().withMessage("The title is required").isLength({m
 await check("description").notEmpty().withMessage("The description is required").run(req)
 console.log(`L a categoria es esta : ${typeof req.body.Category}`)
 await check("category").notEmpty().withMessage("All properties must be categorized").isInt({min:1, max:5}).withMessage("The category is unknown").run(req)
+await check("priceRange").notEmpty().withMessage("All properties must be").isInt({min:1,max:8}).withMessage("The price is unknow").run(req)
+await check("nRooms").notEmpty().withMessage("All properties must be").isInt({min:0,max:10}).withMessage("The number of rooms is unknow").run(req)
+await check("nwc").notEmpty().withMessage("All properties must be").isInt({min:0,max:5}).withMessage("The number of wc is unknow").run(req)
+await check("parkingLot").notEmpty().withMessage("All properties must be").isInt({min:1,max:8}).withMessage("").run(req)
+await check("street").notEmpty().withMessage("All properties must be").isInt({min:1,max:8}).withMessage("").run(req)
 
+console.log()
 }
 
-export {formProperty};
+export {formProperty, saveNewProperty};
