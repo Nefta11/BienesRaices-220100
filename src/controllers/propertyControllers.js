93c132efa276036e624cc3fdd5605b15ff1a6406
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-23 15:04:00 -0600

    Upddate

--- src/controllers/propertyControllers.js
+++ src/controllers/propertyControllers.js
@@ -1,6 +1,7 @@
 import Price from "../models/price.js";
 import Category from "../models/category.js";
 
+import { check } from "express-validator";
 
 const formProperty = async (req,res) =>{
     //TODO: Obtener los precios y categorias actuales para enviarlos a el formulario (vistas)
@@ -12,6 +13,14 @@ const formProperty = async (req,res) =>{
         showHeader:true,
         categories,prices
     });
+}
+
+const saveNewProperty = async (req,res) =>{
+await check("title").notEmpty().withMessage("The title is required").isLength({min:15, max:150}).withMessage("The title property must have between 15 and 150 characters").run(req)
+await check("description").notEmpty().withMessage("The description is required").run(req)
+console.log(`L a categoria es esta : ${typeof req.body.Category}`)
+await check("category").notEmpty().withMessage("All properties must be categorized").isInt({min:1, max:5}).withMessage("The category is unknown").run(req)
+
 }
 
 export {formProperty};
--- src/routes/propertyesRoutes.js
+++ src/routes/propertyesRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 
 router.get('/create',formProperty)
+router.post('/create',saveNewProperty)
 
 export default router
