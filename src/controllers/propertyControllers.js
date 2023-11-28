80a88329620e86e4b1e7faf1ed793e5a74cc2a42
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-27 20:52:59 -0600

    Update...

--- src/controllers/propertyControllers.js
+++ src/controllers/propertyControllers.js
@@ -1,34 +1,79 @@
 import Price from "../models/price.js";
 import Category from "../models/category.js";
+import Property from "../models/property.js";
+import { check, validationResult } from "express-validator";
 
-import { check } from "express-validator";
-
-const formProperty = async (req,res) =>{
-    //TODO: Obtener los precios y categorias actuales para enviarlos a el formulario (vistas)
-
-    const [categories, prices]= await Promise.all([Category.findAll(),Price.findAll()])
-
-    res.render('properties/create.pug',{
-        page:'New property',
-        showHeader:true,
+const formProperty = async (req, res) => {
+    //TODO: Obtener las categorias, y precios actuales para pintarlos en el formulario
+    const [categories, prices] = await Promise.all([Category.findAll(), Price.findAll()])
+    
+    res.render('properties/create.pug', {
+        page: 'New property',
+        showHeader: true,
         categories,
         prices,
         data:req.body
+
     });
+
+
 }
 
-const saveNewProperty = async (req,res) =>{
-await check("title").notEmpty().withMessage("The title is required").isLength({min:15, max:150}).withMessage("The title property must have between 15 and 150 characters").run(req)
-await check("description").notEmpty().withMessage("The description is required").run(req)
-console.log(`La categoria es esta : ${typeof req.body.Category}`)
-await check("category").notEmpty().withMessage("All properties must be categorized").isInt({min:1, max:5}).withMessage("The category is unknown").run(req)
-await check("priceRange").notEmpty().withMessage("All properties must be").isInt({min:1,max:8}).withMessage("The price is unknow").run(req)
-await check("nRooms").notEmpty().withMessage("All properties must be").isInt({min:0,max:10}).withMessage("The number of rooms is unknow").run(req)
-await check("nwc").notEmpty().withMessage("All properties must be").isInt({min:0,max:5}).withMessage("The number of wc is unknow").run(req)
-await check("parkingLot").notEmpty().withMessage("All properties must be").isInt({min:1,max:8}).withMessage("").run(req)
-await check("street").notEmpty().withMessage("All properties must be").isInt({min:1,max:8}).withMessage("").run(req)
+const saveNewProperty = async (req, res) => {
+    
+    await check("title").notEmpty().withMessage("The title is required").isLength({min:15, max:150}).withMessage("The title property must have between 15 and 150 characters").run(req)
 
-console.log( )
+    await check("description").notEmpty().withMessage("The description is required").run(req)
+    console.log(`La categoria es esta que muestro:  ${typeof req.body.category }`)
+
+    await check("category").notEmpty().withMessage("All properties must be categorized").isInt({min:1, max:5}).withMessage("The category is unknown").run(req)
+
+    await check("priceRange").notEmpty().withMessage("All properties must have a price").isInt({min:1, max:8}).withMessage("The price is unknown").run(req)
+
+    await check("nRooms").isInt({min:0, max:10}).withMessage("The number of rooms is unknown").run(req)
+
+    await check("nwc").isInt({min:0, max:5}).withMessage("The number of wc is unknown").run(req)
+
+    await check("parkingLot").isInt({min:0, max:5}).withMessage("The number of parking lot is unknow").run(req)
+
+    await check("street").notEmpty().withMessage("The name of the street is unknow").run(req)
+
+    await check("lat").isFloat({min:-90,max:90}).withMessage("the latitude is not in the requested range").run(req)
+
+    await check("lng").isFloat({min:-180, max: 180}).withMessage("The length is not within the requested range.").run(req)
+
+    //  res.json(validationResult(req))
+
+    let resultValidate = validationResult(req);
+    console.log(`lat: ${req.body.lat}, long: ${req.body.lng}`)
+    let data = req.body
+    console.log(data);
+     
+    const {title, description, category, priceRange, nRooms, nwc, parkingLot, street, lat, lng} =req.body;
+
+    if(resultValidate.isEmpty()){
+        //Creamos
+        const savedProperty = await Property.create({
+            title, description, category, priceRange, rooms:nRooms,wc:nwc, parkinglot:parkingLot, street, lat, lng, price_ID:priceRange, category_ID:category
+        })
+        res.send("Todo bien")
+    }
+    else{
+        const [categories, prices] = await Promise.all([Category.findAll(), Price.findAll()])
+        res.render('properties/create.pug', {
+            page: 'New property',
+            showHeader: true,
+            categories,
+            prices,
+            data:req.body,
+            errors: resultValidate.array(), 
+            propertyData: {
+                title, description, category, priceRange, nRooms, nwc, parkingLot, street, lat, lng
+            },
+    
+        });
+    
+    }
 }
 
-export {formProperty, saveNewProperty};
+export { formProperty, saveNewProperty }
