3a1388f9fe90dcd3d067aecbc17e4dd534f88ca2
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-30 15:53:46 -0600

    Update

--- src/lib/customDropzone.js
+++ src/lib/customDropzone.js
@@ -3,6 +3,10 @@ import {Dropzone} from "dropzone";
 
 Dropzone.options.images = {
     dictDefaultMessage:"Please load the selected images",//Por favor carga tus imagenes
-    acceptedFiles: ".png, .jpg, .jpeg, .vmp, .svg"
+    acceptedFiles: ".png, .jpg, .jpeg, .vmp, .svg",
+    maxFilesize:5,
+    maxFiles:1,
+    paralelUploads:1,
+    autoProccesQueue
 }
 
