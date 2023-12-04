3c3a6eb8932739ab5442ae92abcff60d5cbde094
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-04 12:27:16 -0600

    Update...

--- src/routes/propertyesRoutes.js
+++ src/routes/propertyesRoutes.js
@@ -1,14 +1,14 @@
-import express  from "express";
-import {protectRoute} from "../middlewares/protectRoute.js"
-import {formProperty, saveNewProperty,addImages} from '../controllers/propertyControllers.js'
+import express from "express";
+import { protectRoute } from "../middlewares/protectRoute.js";
+import { formProperty, saveNewProperty, addImage, loadImage } from "../controllers/propertyController.js";
+import uploadImage from "../middlewares/uploadImage.js";
 const router = express.Router();
 
-
-
-
-router.get('/create',protectRoute,formProperty)
-router.get('/addImage',protectRoute,addImages)
-router.post('/create',protectRoute,saveNewProperty)
+ //*EL SEGUNDO PARAMETRO ES EL MIDDLEWARE DE SEGURIDAD
+router.get('/create',protectRoute, formProperty)
+router.post('/create',protectRoute, saveNewProperty)
+ 
+router.get('/addImage/:idProperty', protectRoute,addImage);
+router.post('/addImage/:idProperty', protectRoute, loadImage)
 
 export default router
-
