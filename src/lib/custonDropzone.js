490e6af98cbbcb88f79e9e1aa26f3385b9696d0c
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-05 22:27:23 -0600

    update..

--- src/lib/custonDropzone.js
+++ src/lib/custonDropzone.js
@@ -1,15 +1,31 @@
 import {Dropzone} from "dropzone";
 
-
-Dropzone.options.images = {
-    dictDefaultMessage:"Please load the selected images",//Por favor carga tus imagenes
-    acceptedFiles: ".png, .jpg, .jpeg, .vmp, .svg",
-    maxFilesize:5,
-    maxFiles:1,
-    paralelUploads:1,
-    autoProccesQueue:false,
+Dropzone.options.image = {
+    dictDefaultMessage:"Please load the selected images",
+    acceptedFiles:".png, .jpg, .jpeg, .bmp, .svg",
+    maxFilesize: 5,
+    maxFiles: 1,
+    parallelUploads: 1,
+    autoProcessQueue: false,
     addRemoveLinks: true,
-    //dictRemoveFile:"Borrar fotografia"
-   // dictMaxFilesExceeded:"Solamente una imagen"
+    //dictRemoveFile:"Borrar fotografia" para español
+    //dictMaxFilesExceeded: "Solamente uno"
+    headers:{
+
+    },
+    paramName:'image',
+    //funcion cuando inicia dropzone
+    init: function(){
+        const Dropzone = this
+        const btnPublish = document.querySelector('#publish')
+        btnPublish.addEventListener('click', function() {
+            Dropzone.processQueue();
+        });
+        Dropzone.on('queuecomplete', function(){
+            if(Dropzone.getActiveFiles().length == 0){
+                window.location.href = '/home'
+            }
+        })
+    }
 }
 
